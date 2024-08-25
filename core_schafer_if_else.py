fb37043a0f31bf02fc2ff2f37d6af1886b65169d
Proshenzit Roy <37993972+Proshenzit@users.noreply.github.com>
2024-08-25 04:06:07 -0700

    Add files via upload

---
+++ core_schafer_if_else.py
@@ -0,0 +1,19 @@
+'''
+language ="python"
+if language  == "python":
+    print("language is python ")
+elif language =="java":
+    print("language is java ")
+else:
+    print("no match ")
+'''
+
+user ="admin"
+log_in = True
+
+
+if not log_in:
+
+    print("Please Login  ")
+else:
+    print("Welcome ")
